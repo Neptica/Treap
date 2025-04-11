fb0be5e08fed6be47d205e13acab6da1cd7982f2
Michael Mumme <neptica@hotmail.com>
2025-04-10 19:15:16 -0500

    fix improper Heap comparison between strings
    9 would be seen as larger than 47 without conversion to an integer

--- Node.py
+++ Node.py
@@ -1,7 +1,7 @@
 class Node:
     def __init__(self, key, val):
         self.key = key
-        self.priority = f"{val*100:.0f}"  # for value 0-99
+        self.priority = int(f"{val*100:.0f}")  # for value 0-99
         self.left = None
         self.right = None
         self.parent = None
